06b990af02fbb1c6a313622aef02c98eb23c84cf
benryves <benryves@benryves.com>
2023-04-13 02:12:17 +0100

    runapp now supports local ozvm
    If local (modified) ..\ozvm\z88.jar is not present it'll use the release in z88\bin\z88.jar

--- runapp.cmd
+++ runapp.cmd
@@ -3,7 +3,11 @@
 call makeapp
 if %ERRORLEVEL% neq 0 goto FAIL
 
-start ..\ozvm\z88.jar com COM2 crd2 32 27C VDriveZ88.epr
+if exist ..\ozvm\z88.jar (
+	start ..\ozvm\z88.jar com COM2 crd2 32 27C VDriveZ88.epr
+) else (
+	start z88\bin\z88.jar crd2 32 27C VDriveZ88.epr
+)
 goto END
 
 :FAIL
