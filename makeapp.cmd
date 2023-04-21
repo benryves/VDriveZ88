ea43676f313b79e0e7a5bbfe3a99fbeb2df3b079
benryves <benryves@benryves.com>
2023-04-21 13:25:26 +0100

    Removed references to "lockup" from build scripts

--- makeapp.cmd
+++ makeapp.cmd
@@ -5,13 +5,13 @@ del /Q *.obj *.sym *.bin *.map *.6? *.epr 2>nul >nul
 :: return version of Mpm to command line environment.
 :: Only V1.5 or later of Mpm supports macros
 mpm -version 2>nul >nul
-if ERRORLEVEL 15 goto COMPILE_LOCKUP
+if ERRORLEVEL 15 goto COMPILE
 echo Mpm version is less than V1.5, compilation aborted.
 echo Mpm displays the following:
 mpm
 goto FAIL
 
-:COMPILE_LOCKUP
+:COMPILE
 
 :: Assemble the application
 mpm -b -I.\z88\oz\def VDriveZ88.asm
