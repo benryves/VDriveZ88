4f1438638383e1c9c01b3ffed61ffe3b6202e4e8
benryves <benryves@benryves.com>
2023-04-30 02:24:15 +0100

    Fixed runapp.cmd for new .epr filename

--- runapp.cmd
+++ runapp.cmd
@@ -4,9 +4,9 @@ call makeapp
 if %ERRORLEVEL% neq 0 goto FAIL
 
 if exist ..\ozvm\z88.jar (
-	start ..\ozvm\z88.jar com COM2 crd2 32 27C VDriveZ88.epr
+	start ..\ozvm\z88.jar com COM2 crd2 32 27C VDrive.epr
 ) else (
-	start z88\bin\z88.jar crd2 32 27C VDriveZ88.epr
+	start z88\bin\z88.jar crd2 32 27C VDrive.epr
 )
 goto END
 
